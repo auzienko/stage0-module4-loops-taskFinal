b02d20df86b8aa63581561959e7394d1e2c846ef
Aleksandr Uzienko <a.uzienko@gmail.com>
2023-05-10 19:47:46 +0300

    solution

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,8 +2,26 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 0; j < cathetusLength * 2 - 1; j++) {
 
+                int value = cathetusLength - j;
+                int correctedValue = value > 0 ? value : (-value) + 2;
+
+                if (correctedValue > i) {
+                    if (value > 0) {
+                        System.out.print(' ');
+                    } else {
+                        break;
+                    }
+                } else {
+                    System.out.print(correctedValue);
+                }
+            }
+            System.out.println();
+        }
     }
+
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
